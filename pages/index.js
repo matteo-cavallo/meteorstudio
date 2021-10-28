05d968b4dd70d261936556ddc0cad68821138645
matteocavallo <mcavallo@soldo.com>
2021-10-28 11:18:30 +0200

    Fix files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
-import Hero from "./components/Hero";
 
 export default function Home() {
   return (
